813baea21077b88685398d739308b3ed2a2fefc2
JaviFdez7 <javidelatorre400@gmail.com>
2023-10-11 00:34:10 +0200

    comments added and console logs out

--- src/githubGQLPaginatorService.js
+++ src/githubGQLPaginatorService.js
@@ -1,111 +1,27 @@
 import axios from 'axios';
 import fs from 'fs';
-import { graphQlQueryToJson } from 'graphql-query-to-json'; //npm install graphql-query-to-json
-import { jsonToGraphQLQuery } from 'json-to-graphql-query'; //npm install json-to-graphql-query
+import { graphQlQueryToJson } from 'graphql-query-to-json'; // npm install graphql-query-to-json
+import { jsonToGraphQLQuery } from 'json-to-graphql-query'; // npm install json-to-graphql-query
 
+  // Assumptions: ARRAYS ONLY EXIST IN NODES, An array is not contemplated elsewhere
+  // WHEN AN OBJECT HAS NODES AS A PROPERTY, IT IS CONSIDERED A PAGEABLE TYPE
+  // PAGEABLE TYPES ARE NOT CONTEMPLATED IN A SIMPLE TYPE
 
-  // Supuestos: SOLO EXISTEN ARRAYS EN NODES, No se contempla un array en otro sitio
-  // CUANDO UN OBJETO TIENE COMO PROPIEDAD NODES SE CONSIDERA UN META OBJETOç
-  //NO SE CONTEMPLA METATIPO DE UN TIPO SIMPLE
-
-  /*
-  {
-        repository(name: "${name}", owner:"${owner}") {
-          issues(first: 25) {
-            totalCount
-            pageInfo {
-              hasNextPage
-              endCursor
-            }
-            nodes {
-              id
-              title
-            }
-          }
-          pullRequests(first: 10, states: MERGED) {
-            totalCount
-            pageInfo {
-              hasNextPage
-              endCursor
-            }
-            nodes {
-              id
-              state
-              title
-              body
-              baseRefName
-              headRefName
-              createdAt
-              author {
-                login
-              }
-              mergedAt
-              mergedBy {
-                login
-              }
-              reviews(first: 1) {
-                totalCount
-                pageInfo {
-                  hasNextPage
-                  endCursor
-                }
-                nodes {
-                  id
-                  createdAt
-                  state
-                  reactions (first: 1) {
-                    totalCount
-                    pageInfo {
-                      hasNextPage
-                      endCursor 
-                    }
-                    nodes {
-                      content
-                      id
-                      user{
-                        name
-                      }
-                    }
-                  }
-                }
-              }
-              assignees(first: 1){
-                pageInfo {
-                  hasNextPage
-                  endCursor
-                }
-                totalCount
-                nodes{
-                  id
-                  name
-                }
-              }
-            }
-          }
-        }
-      }
-  */
-
-
-  // Public function ------------------------------------------------------------------------------------
+  // Public function --------------------------------------------------------------------------------------------------
   export async function githubGQLPaginator(query, token){
     tokenPred = token;
     originalQuery = query;
 
     const result = await requestQuery(query);
-    console.log("PREVIEW STRINGYFY RESULT: ", result)
-    console.log("PREVIEW PARSE RESULT: ", JSON.parse(result))
+
     return await pagination(JSON.parse(result));
   }
 
 
-  //Auxiliar function ---------------------------------------------------------------------------------
+  // Auxiliar function --------------------------------------------------------------------------------------------------
   async function pagination(result) {
 
-    await resolveMtypePagesRecursive(result, "repository");
-
-    console.log(result);
-    console.log(result.repository.issues.nodes.length);
+    await resolvePtypePagesRecursive(result, "repository");
 
     const finalResultJSON = JSON.stringify(result, null, 2);
 
@@ -123,138 +39,116 @@ import { jsonToGraphQLQuery } from 'json-to-graphql-query'; //npm install json-t
   }
 
 
-  // Global variables ------------------------------------------------------------------------------------
+  // Global variables -----------------------------------------------------------------------------------------------------
   var originalQuery = "";
 
   var tokenPred = "";
 
-  var mtypePath = [];
+  var ptypePath = [];
 
-  var lastVisitedIdByMtype = {};
+  var lastVisitedIdByPtype = {};
 
 
   // Recursive function --------------------------------------------------------------------------------------------------
-  async function resolveMtypePagesRecursive(currentResult, rootAttribute) {
-    
-    console.log("ATRIBUTO RAIZ: ", rootAttribute)
-    
+  async function resolvePtypePagesRecursive(currentResult, rootAttribute) {
+      
     const properties = []
-    for (const property in currentResult) { //Recopila todas las claves del resultado
+    for (const property in currentResult) { // Collect all keys from the result
       if(property!=="pageInfo" && property!=="totalCount" && typeof currentResult[property]!=="string" || property=="id")
         properties.push(property);
     }
 
-    if(properties.includes("id")){ //Almacena las última ids de cada metatipo que se ha visitado
-      lastVisitedIdByMtype[mtypePath[mtypePath.length-1]] = currentResult.id;
+    if(properties.includes("id")){ // Stores the last ids of each pageable type that has been visited
+      lastVisitedIdByPtype[ptypePath[ptypePath.length-1]] = currentResult.id;
     }
     
-    if(properties.length == 0){ //Caso base
-      console.log("CASO BASE")
+    if(properties.length == 0){ // Base case
       return;
     }
 
-    console.log(`RESULT CONTIENE LAS SIGUIENTE PROPIEDADES ${properties}`);
-
-    if(properties.includes("nodes")){ //Comprueba si se puede realizar paginación
-      if(!mtypePath.includes(rootAttribute)) mtypePath.push(rootAttribute) //Almacena la ruta a seguir al encontrar un metatipo
+    if(properties.includes("nodes")){ // Check if pagination can be performed
+      if(!ptypePath.includes(rootAttribute)) ptypePath.push(rootAttribute) // Stores the path to follow when finding a pageable type
       const nodesHasMorePages = currentResult?.pageInfo?.hasNextPage;
       const totalCountExist = currentResult.hasOwnProperty("totalCount");
       const endCursorExist = currentResult.pageInfo?.hasOwnProperty("endCursor");
       const nodesHasIdProperty = currentResult.nodes?.[0]?.hasOwnProperty("id");
       if( nodesHasMorePages && totalCountExist && endCursorExist && nodesHasIdProperty){
-        console.log("ESTE ATRIBUTO ES PAGINALBLE: ", rootAttribute)
         const cursor = currentResult.pageInfo.endCursor;
-        console.log("RESULTADO ANTIGUO: ", currentResult)
-        console.log(currentResult.nodes.length)
-        const ghostNodes = await getAllGhostNodes(cursor); //Resuelve la paginación
-
+        const ghostNodes = await getAllGhostNodes(cursor); // Solve pagination
         currentResult.nodes.push(...ghostNodes);
-        
-        console.log("NUEVO RESULTADO: ", currentResult)
-        console.log(currentResult.nodes.length)
-
       }
     }
     
-    if(properties.includes("nodes")){ //recorre todos los nodos cuando encontramos un metatipo
-      console.log("NODES ENCONTRADO: ", currentResult.nodes.length)
+    if(properties.includes("nodes")){ // Loop through all nodes when we find a pageable type
       for(const node of currentResult.nodes){
         var subResult = node;
-        console.log(subResult)
-        await resolveMtypePagesRecursive(subResult, "nodes");
+        await resolvePtypePagesRecursive(subResult, "nodes");
       }
-      mtypePath.pop(); //Elimina el ultimo elemento de la ruta porque ya hemos recorrido todos los nodos y ya volvemos hacia atras
+      ptypePath.pop(); // Delete the last element of the path because we have already gone through all the nodes and we are already going back
       return;
     }
 
-    //Recorre todas la propiedades encontradas
+    // Browse all found properties
     for(const property of properties){
       if(currentResult.hasOwnProperty(property)){
         var subResult = currentResult[property];
-        console.log("COMPROBANDO ", property)
-        await resolveMtypePagesRecursive(subResult, property);
+        await resolvePtypePagesRecursive(subResult, property);
       }
     }
     return;
   }
   
 
-  // Ghost Nodes -----------------------------------------------------------------------------------------
+  // Ghost Nodes --------------------------------------------------------------------------------------------------
   async function getAllGhostNodes(cursorOfNextPage){
-    console.log("RUTA: ", mtypePath)
     var hasNextPage = true;
     var finalGhostNodes = [];
 
-    console.log(originalQuery)
-
     var originalQueryObj = graphQlQueryToJson(originalQuery) // Used inside an eval()
 
-    const firstLevelMtype = mtypePath.length == 1;
+    const firstLevelPtype = ptypePath.length == 1;
 
-    if(firstLevelMtype){
-      var getNextPageQuery = originalQuery.replace(new RegExp(`${mtypePath[0]}\\(`, 'g'), `${mtypePath[0]}(after: "#NEWCURSOR#", `);
+    if(firstLevelPtype){
+      var getNextPageQuery = originalQuery.replace(new RegExp(`${ptypePath[0]}\\(`, 'g'), `${ptypePath[0]}(after: "#NEWCURSOR#", `);
       while(hasNextPage){
-        console.log(getNextPageQuery.replace(/#NEWCURSOR#/g, cursorOfNextPage));
 
         var nextPageResult = await requestQuery(getNextPageQuery.replace(/#NEWCURSOR#/g, cursorOfNextPage))
 
-        console.log(nextPageResult)
-
         var nextPageResultJSON = JSON.parse(nextPageResult);
 
-        var newGhostNodes = nextPageResultJSON.repository[mtypePath[mtypePath.length-1]].nodes;
+        var newGhostNodes = nextPageResultJSON.repository[ptypePath[ptypePath.length-1]].nodes;
 
-        if(nextPageResultJSON.repository[mtypePath[0]].pageInfo.hasNextPage){
-          cursorOfNextPage = nextPageResultJSON.repository[mtypePath[0]].pageInfo.endCursor;
+        if(nextPageResultJSON.repository[ptypePath[0]].pageInfo.hasNextPage){
+          cursorOfNextPage = nextPageResultJSON.repository[ptypePath[0]].pageInfo.endCursor;
         } else{
             hasNextPage = false;
         }
 
         finalGhostNodes.push(...newGhostNodes);
+        
       }
-    } else if(!firstLevelMtype){
+    } else if(!firstLevelPtype){
       while(hasNextPage){
-        var metatypeRuteString = "";
-        for(var i = 0; i<mtypePath.length-1;i++){
-          const lastElement = mtypePath[i];
+
+        var ptypeRuteString = "";
+
+        for(var i = 0; i<ptypePath.length-1;i++){
+          const lastElement = ptypePath[i];
           const capitalizeFirstCharAndDeleteLastChar = lastElement.charAt(0).toUpperCase() + lastElement.slice(1, -1);
-          metatypeRuteString += capitalizeFirstCharAndDeleteLastChar;
+          ptypeRuteString += capitalizeFirstCharAndDeleteLastChar;
         }
 
-        var nodeId = lastVisitedIdByMtype[mtypePath[mtypePath.length-2]] //Antepenultimo metatipo de la ruta
+        var nodeId = lastVisitedIdByPtype[ptypePath[ptypePath.length-2]] // Second to last pageable type of the path
 
-        //nodesQuery = getNodeSectionFromQuery();
-        var pathIncludingNodes = mtypePath.map(element => `${element}.nodes`).join(".")
+        var pathIncludingNodes = ptypePath.map(element => `${element}.nodes`).join(".")
         var nodesProperties = jsonToGraphQLQuery(eval("originalQueryObj.query.repository."+pathIncludingNodes), { pretty: true });
 
-        console.log("PROPERTIES NODES: ", nodesProperties);
-
         var getNextPageQuery =     
-        `query Get${metatypeRuteString}ById {
+        `query Get${ptypeRuteString}ById {
           node(id: \"${nodeId}\") { 
-            ... on ${metatypeRuteString} {
+            ... on ${ptypeRuteString} {
               id
-              ${mtypePath[mtypePath.length-1]}(first: 1, after: \"${cursorOfNextPage}\"){
+              ${ptypePath[ptypePath.length-1]}(first: 1, after: \"${cursorOfNextPage}\"){
                 pageInfo {
                   hasNextPage
                   endCursor
@@ -268,16 +162,14 @@ import { jsonToGraphQLQuery } from 'json-to-graphql-query'; //npm install json-t
           }
         }`
 
-        console.log(getNextPageQuery);
-
         var nextPageResult = await requestQuery(getNextPageQuery);
 
         var nextPageResultJSON = JSON.parse(nextPageResult);
 
-        var newGhostNodes = nextPageResultJSON.node[mtypePath[mtypePath.length-1]].nodes;
+        var newGhostNodes = nextPageResultJSON.node[ptypePath[ptypePath.length-1]].nodes;
 
-        if(nextPageResultJSON.node[mtypePath[mtypePath.length-1]].pageInfo.hasNextPage){
-          cursorOfNextPage = nextPageResultJSON.node[mtypePath[mtypePath.length-1]].pageInfo.endCursor;
+        if(nextPageResultJSON.node[ptypePath[ptypePath.length-1]].pageInfo.hasNextPage){
+          cursorOfNextPage = nextPageResultJSON.node[ptypePath[ptypePath.length-1]].pageInfo.endCursor;
         } else{
           hasNextPage = false;
         }
@@ -290,7 +182,7 @@ import { jsonToGraphQLQuery } from 'json-to-graphql-query'; //npm install json-t
   }
 
 
-  //  Request GQL query ----------------------------------------------------------------------
+  //  Request GQL query --------------------------------------------------------------------------------------------------
   async function requestQuery(query) {
     const apiUrl = 'https://api.github.com/graphql';
     const requestConfig = {
@@ -302,7 +194,6 @@ import { jsonToGraphQLQuery } from 'json-to-graphql-query'; //npm install json-t
     try {
       const result = await axios.post(apiUrl, { query }, requestConfig);
 
-      // Verificar si la respuesta es exitosa (código de estado 200)
       if (result.status === 200) {
         const responseData = result.data.data;
 
@@ -310,10 +201,9 @@ import { jsonToGraphQLQuery } from 'json-to-graphql-query'; //npm install json-t
           throw new Error('El repositorio no fue encontrado');
         }
 
-        // Convertir los datos a JSON utilizando JSON.stringify
         const repositoryJSON = JSON.stringify(responseData);
 
-        return repositoryJSON; // Devuelve los datos como JSON
+        return repositoryJSON;
       } else {
         throw new Error(`Error en la solicitud: ${result.statusText}`);
       }
